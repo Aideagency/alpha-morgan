153b62b61856d6af1130a09faea33b02c2f2d8b6
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2020-12-03 13:27:52 +0100

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Footer -->
+  <footer class="page-footer font-small stylish-color-dark my-4">
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left footer-about">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-4 mx-auto">
+          <!-- Content -->
+          <h3 class="mt-3 mb-4 text-orange">
+            Contact Us
+          </h3>
+          <p class="text-white">
+            For instant enquiry, do visit our office address below or send us an
+            email or call us.
+          </p>
+          <p class="text-white">
+            Union Bank Building (2nd Floor) Plot 1668b, Oyin Jolayemi Street,
+            Victoria Island, Lagos. +234 01-454 2428, 081 51 71 71 71
+            info@alphamorgan.com
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-2 mx-auto">
+          <!-- Links -->
+          <h4 class="mt-3 mb-4 text-orange">
+            News Highlights
+          </h4>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-white"
+                >Daily News // 13th Nov, 2020 Read details →
+              </a>
+            </li>
+            <li>
+              <a href="#!" class="text-white"
+                >Daily News // 12th Nov, 2020 Read details →
+              </a>
+            </li>
+            <li>
+              <a href="#!" class="text-white"
+                >Daily News // 11th Nov, 2020 Read details →</a
+              >
+            </li>
+          </ul>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+    <!-- Footer Links -->
+  </footer>
+  <!-- Footer -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer-about {
+  background: linear-gradient(0deg, rgba(10, 9, 9, 0.5), rgba(0, 0, 0, 0.9)),
+    url("./../assets/img/footer-bg.png");
+}
+</style>
