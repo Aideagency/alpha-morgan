35b54dbf1defe52bc29f5c545468c85c37bcdb33
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2020-12-03 17:05:58 +0100

    created client portal page and routing

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/portal",
+    name: "Portal",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ClientPortal.vue")
   }
 ];
 
---
+++ src/views/ClientPortal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="portal">
+    <aside>
+      <img src="" alt="" />
+      <p>Username</p>
+      <p>ID 34657</p>
+      <button>Sign out</button>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
